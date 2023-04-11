c4abf469c89b2043e91c99a31e2fd74abe0e9338
eduardo7321 <eduardo.jose184@gmail.com>
2023-04-10 23:36:45 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.domonio.Curso;
+import br.com.dio.desafio.domonio.Mentoria;
+
+import java.time.LocalDate;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("descrição curso javascropt");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
